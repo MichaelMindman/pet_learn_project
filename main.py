9e17c44f9bb3e4406605d2d16774b7c7aa48ba0e
Mikhail Saraev <MixailR@mail.ru>
2026-09-23 22:56:58 +0300

    Module  upgraid: add function

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import requests
 
 
+def train_hot_key():
+
+    print("train_hot_keys")
+    print("train_hot_keys")
+    print("train_hot_keys")
+
+
 def main():
     response = requests.get("https://api.github.com")
     print(f"Status: {response.status_code}")
     print("Hello from virtual enviroment!")
+
+    train_hot_key()
 
 
 if __name__ == "__main__":
