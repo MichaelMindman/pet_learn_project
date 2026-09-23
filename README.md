41e0358be443a529dc4e41fa573e0a6e68e6752b
Mikhail Saraev <MixailR@mail.ru>
2026-09-23 16:05:58 +0300

    Add GitHub API example

---
+++ README.md
@@ -0,0 +1,11 @@
+# pet_learn_project
+Учебный проект для изучения Python venv, Git, terminal.
+
+## Установка
+
+    python - m venv venv
+    source venv/Scripts/Activate
+    pip install -r requirements.txt
+
+## Запуск
+    python main.py
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import requests
+
+
 def main():
+    response = requests.get("https://api.github.com")
+    print(f"Status: {response.status_code}")
     print("Hello from virtual enviroment!")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
