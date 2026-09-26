9b17fb4f639e53536522e9343f0d7ffab4550644
Mikhail Saraev <MixailR@mail.ru>
2026-09-27 00:06:17 +0300

    update:sys_script_1.py, after practice hot keys

--- sys_script/sys_script_1.py
+++ sys_script/sys_script_1.py
@@ -9,7 +9,9 @@ print("Module sys.script_1 is run")
 # print(dir(sys.modules))
 # print()
 # print(dir(sys.modules.__doc__))
+
 # print(help(dir))
+
 
 public = [s for s in dir(sys) if not s.startswith("_")]
 private = [s for s in dir(sys) if s.startswith("_") and not s.startswith("__")]
@@ -19,3 +21,7 @@ print()
 print(private, len(private))
 print()
 print(dunder, len(dunder))
+
+print()
+public_2 = sorted(public)
+print(public_2)
